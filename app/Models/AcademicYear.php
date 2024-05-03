a93d46b261fc2b4938717c12a807bba7d7f3189f
yudhaph0300 <yudhapamungkas0300@gmail.com>
2024-05-04 04:02:01 +0700

    fix feature for academic year

--- app/Models/AcademicYear.php
+++ app/Models/AcademicYear.php
@@ -8,8 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class AcademicYear extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'name',
-        'semester'
+    protected $guarded = [
+        'id',
     ];
+
+    public function classname()
+    {
+        return $this->hasMany(ClassName::class);
+    }
+
+    public function lesson()
+    {
+        return $this->hasMany(Lesson::class);
+    }
 }
