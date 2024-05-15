291ff6feef9df4e28131c4601a6c2f9ad6b31937
yudhaph0300 <yudhapamungkas0300@gmail.com>
2024-05-16 03:57:28 +0700

    add feature for show raport

--- app/Http/Controllers/RaportController.php
+++ app/Http/Controllers/RaportController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AcademicYear;
+use App\Models\ClassName;
 use App\Models\LessonValue;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class RaportController extends Controller
@@ -10,10 +13,37 @@ class RaportController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         $title = 'cetak raport';
-        return view('pages.admin.raport.index', compact('title'));
+        $data_class = ClassName::where('id_academic_year', session()->get('id_academic_year'))->get();
+
+        // Simpan nilai filter dalam sesi
+        session()->put('raport_filter_kelas', $request->kelas);
+        session()->put('raport_filter_search', $request->search);
+
+        // Filter data siswa berdasarkan kelas dan pencarian
+        $query = Student::query();
+
+
+        // Filter berdasarkan kelas
+        if (session()->has('raport_filter_kelas')) {
+            if (session('raport_filter_kelas') === '__NULL__') {
+                $query->whereNull('id_class');
+            } else {
+                $query->where('id_class', session('raport_filter_kelas'));
+            }
+        }
+
+
+        // Filter berdasarkan pencarian
+        if (session()->has('raport_filter_search')) {
+            $query->where('name', 'like', '%' . session('raport_filter_search') . '%');
+        }
+
+        $data_student = $query->get();
+
+        return view('pages.admin.raport.index', compact('title', 'data_class', 'data_student'));
     }
 
     /**
@@ -35,9 +65,19 @@ class RaportController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($studentId)
     {
-        //
+        $title = 'cetak raport';
+        $student = Student::findOrFail($studentId);
+        $academic_year = AcademicYear::findOrFail(session()->get('id_academic_year'));
+        $raport = LessonValue::select('lessons.name as lesson_name', 'lesson_values.ko1', 'lesson_values.ko2', 'lesson_values.sub1', 'lesson_values.sub2', 'lesson_values.praktik', 'lesson_values.uts_uas')
+            ->join('learnings', 'lesson_values.id_learning', '=', 'learnings.id')
+            ->join('lessons', 'learnings.id_lesson', '=', 'lessons.id')
+            ->where('lesson_values.id_student', $studentId)
+            ->where('learnings.id_academic_year', session()->get('id_academic_year'))
+            ->get();
+
+        return view('pages.admin.raport.show', compact('title', 'raport', 'student', 'academic_year'));
     }
 
     /**
--- resources/views/pages/admin/raport/index.blade.php
+++ resources/views/pages/admin/raport/index.blade.php
@@ -14,11 +14,43 @@
 
             <div class="card shadow mt-3">
                 <div class="card-body">
-                    <div class="text-end mb-3">
-                        <button class="btn btn-outline-primary rounded-pill" data-bs-toggle="modal"
-                            data-bs-target="#modal-tambah">
-                            <i class="bi bi-person-add me-2"></i>Tambah Tahun Pelajaran
-                        </button>
+
+                    <div class="mb-3">
+
+                        <form class="row align-items-end g-3" method="GET" action="{{ url('/admin/cetak-raport') }}">
+                            @csrf
+                            <div class="col-md-3">
+                                <div class="">
+                                    <select id="kelas" name="kelas" class="form-select custom-select"
+                                        aria-label="Default select example">
+                                        <option value="" {{ session('raport_filter_kelas') == '' ? 'selected' : '' }}>
+                                            Semua
+                                            Kelas
+                                        </option>
+                                        @foreach ($data_class as $class)
+                                            <option value="{{ $class->id }}"
+                                                {{ session('raport_filter_kelas') == $class->id ? 'selected' : '' }}>
+                                                {{ $class->name }}</option>
+                                        @endforeach
+                                    </select>
+
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="">
+                                    <input type="text" value="{{ session('raport_filter_search') }}"
+                                        class="form-control custom-search" id="search" name="search"
+                                        placeholder="Masukan keyword">
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="">
+                                    <button type="submit" class="btn btn-primary btn-filter w-100"><i
+                                            class="bi bi-filter me-2"></i>Filter</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
 
                     <table class="table table-bordered table-hover">
@@ -26,22 +58,34 @@
                             <tr>
 
                                 <th scope="col" class="text-center">No</th>
+                                <th scope="col" class="text-center">NIS</th>
                                 <th scope="col" class="text-center">Nama siswa</th>
                                 <th scope="col" class="text-center">Kelas</th>
                                 <th scope="col" class="text-center">Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td scope="col" class="text-center" style="vertical-align: middle;">1</td>
-                                <td scope="col" class="text-center" style="vertical-align: middle;">Nama siswa</td>
-                                <td scope="col" class="text-center" style="vertical-align: middle;">Kelas</td>
-                                <td scope="col" class="text-center" style="vertical-align: middle;">
-                                    <button class="btn btn-primary">
-                                        Lihat Raport
-                                    </button>
-                                </td>
-                            </tr>
+                            @foreach ($data_student as $index => $item)
+                                <tr>
+                                    <td scope="col" class="text-center" style="vertical-align: middle;">
+                                        {{ $index + 1 }}</td>
+                                    <td scope="col" class="text-center" style="vertical-align: middle;">
+                                        {{ $item->nis }}</td>
+                                    <td scope="col" style="vertical-align: middle;">
+                                        {{ $item->name }}</td>
+                                    <td scope="col" class="text-center" style="vertical-align: middle;">
+                                        {{ $item->classname->name }}</td>
+                                    <td scope="col" class="text-center" style="vertical-align: middle;">
+
+                                        <a href="/admin/cetak-raport/{{ $item->id }}" class="btn btn-primary">
+                                            Lihat
+                                        </a>
+                                        <button class="btn btn-primary">
+                                            Cetak
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
 
 
                         </tbody>
