5370f3b18aa25bb04791dbca5bf0822d786078f8
yudhaph0300 <yudhapamungkas0300@gmail.com>
2024-05-27 21:30:51 +0700

    update js for alert

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,8 +1,107 @@
+// For sidebar
 document.addEventListener("DOMContentLoaded", function () {
     var sidebar = document.getElementById("sidebar");
     var btnHamburger = document.getElementById("btn-hamburger");
 
     btnHamburger.addEventListener("click", function () {
         sidebar.classList.toggle("sidebar-active");
+    });
+});
+
+// Alert confirmation add student
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("add-student-form");
+    form.addEventListener("submit", function (event) {
+        const confirmation = confirm("Apakah anda ingin menambahkan siswa?");
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
+});
+
+// Alert confirmation edit student
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("edit-student-form");
+    form.addEventListener("submit", function (event) {
+        const confirmation = confirm("Apakah anda ingin mengupdate siswa?");
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
+});
+
+// Alert confirmation add teacher
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("add-teacher-form");
+    form.addEventListener("submit", function (event) {
+        const confirmation = confirm("Apakah anda ingin menambah guru?");
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
+});
+
+// Alert confirmation edit teacher
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("edit-teacher-form");
+    form.addEventListener("submit", function (event) {
+        const confirmation = confirm("Apakah anda ingin mengupdate guru?");
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
+});
+
+// Alert confirmation add class
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("add-class-form");
+    form.addEventListener("submit", function (event) {
+        const confirmation = confirm("Apakah anda ingin menambahkan kelas?");
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
+});
+
+// Alert confirmation edit class
+document.addEventListener("DOMContentLoaded", function () {
+    const forms = document.querySelectorAll(".edit-class-form");
+    forms.forEach((form) => {
+        form.addEventListener("submit", function (event) {
+            const confirmation = confirm(
+                "Apakah anda ingin mengupdate data kelas?"
+            );
+            if (!confirmation) {
+                event.preventDefault();
+            }
+        });
+    });
+});
+
+// Alert confirmation add mapel
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("add-mapel-form");
+    form.addEventListener("submit", function (event) {
+        const confirmation = confirm(
+            "Apakah anda ingin menambahkan mata pelajaran?"
+        );
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
+});
+
+// Alert confirmation edit mapel
+document.addEventListener("DOMContentLoaded", function () {
+    const forms = document.querySelectorAll(".edit-mapel-form");
+    forms.forEach((form) => {
+        form.addEventListener("submit", function (event) {
+            const confirmation = confirm(
+                "Apakah anda ingin mengupdate data mata pelajaran?"
+            );
+            if (!confirmation) {
+                event.preventDefault();
+            }
+        });
     });
 });
